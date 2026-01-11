dd6d75429c9e1864da0fcb3e539afd97039c5b38
uasuna2022 <max.gomanchuk@gmail.com>
2026-01-12 00:07:20 +0100

    Project4 initial commit

---
+++ Project4_Graphics3DApplication/Project4_Graphics3DApplication/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+
---
+++ Project4_Graphics3DApplication/Project4_Graphics3DApplication/main.cpp
@@ -0,0 +1,129 @@
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include "config.h"
+#include "shaders.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "3D Graphics Application", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+	
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	float vertices1[] = {
+		// positions           // colors
+		1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.0f, // v0 - red
+		-1.0f, -1.0f,  1.0f,    0.0f, 1.0f, 0.0f, // v1 - green
+		-1.0f,  1.0f, -1.0f,    0.0f, 0.0f, 1.0f, // v2 - blue
+		1.0f, -1.0f, -1.0f,    1.0f, 1.0f, 0.0f  // v3 - yellow
+	};
+
+	unsigned int indices1[] = {
+					0, 1, 2,
+					0, 3, 1,
+					0, 2, 3,
+					1, 3, 2
+	};
+
+	unsigned int VBO1, VAO1, EBO1;
+
+	glGenVertexArrays(1, &VAO1);
+	glGenBuffers(1, &VBO1);
+	glGenBuffers(1, &EBO1);
+
+	glBindVertexArray(VAO1);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO1);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices1), indices1, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(0);
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+
+		glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
+
+		glm::mat4 model = glm::mat4(1.0f);
+		float angle = (float)glfwGetTime() * 50.0f; 
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 1.0f, 0.0f));
+
+		int modelLoc = glGetUniformLocation(shaderProgram, "model");
+		int viewLoc = glGetUniformLocation(shaderProgram, "view");
+		int projLoc = glGetUniformLocation(shaderProgram, "projection");
+
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		glBindVertexArray(VAO1);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &VAO1);
+	glDeleteBuffers(1, &VBO1);
+	glDeleteBuffers(1, &EBO1);
+	glDeleteProgram(shaderProgram);
+
+	glfwTerminate();
+	return 0;
+
+}
